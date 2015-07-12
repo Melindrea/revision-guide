a2060659f2ddfb0cfea18240459c530053d384f5
Marie Hogebrandt <marie.hogebrandt@pineberry.com>
2015-07-12 21:12:09 +0200

    Lesson 2.7

--- generator.py
+++ generator.py
@@ -1,13 +1,11 @@
 import os
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, abort
 from werkzeug import cached_property
 import markdown
 import yaml
 
 POSTS_FILE_EXTENSION = '.md'
-
-app = Flask(__name__)
 
 class Post(object):
     def __init__(self, path, root_dir=''):
@@ -43,7 +41,42 @@ class Post(object):
     def url(self):
         return url_for('post', path=self.url_path)
 
+class Blog(object):
+    def __init__(self, app, root_dir, file_ext=POSTS_FILE_EXTENSION):
+        self.root_dir = root_dir
+        self.file_ext = file_ext
+        self._app = app
+        self._cache = {}
+        self._initialise_cache()
 
+    @property
+    def posts(self):
+        return self._cache.values()
+
+    def get_post_or_404(self, path):
+        """ Returns the post object for given path, or raises a NotFound exception
+        """
+
+        try:
+            return self._cache[path]
+        except KeyError:
+            abort(404)
+
+    def _initialise_cache(self):
+        """ Walks the root directory and adds all posts to the cache
+        """
+
+        for (root, dirpaths, filepaths) in os.walk(self.root_dir):
+            for filepath in filepaths:
+                filename, ext = os.path.splitext(filepath)
+
+                if ext == self.file_ext:
+                    path = os.path.join(root, filepath).replace(self.root_dir, '')
+                    post = Post(path, root_dir=self.root_dir)
+                    self._cache[post.url_path] = post
+
+app = Flask(__name__)
+blog = Blog(app, root_dir='posts')
 
 @app.template_filter('date')
 def format_date(value, format='%B %d, %Y'):
@@ -59,16 +92,14 @@ def format_date(value, format='%B %d, %Y'):
 
 @app.route('/')
 def index():
-    posts = [Post('hello.md', root_dir='posts')]
-
-    return render_template('index.html', posts=posts)
+    return render_template('index.html', posts=blog.posts)
 
 @app.route('/blog/<path:path>')
 def post(path):
     # raise # Breakpoint for werkzeug, browser-based
     # import ipdb; ipdb.stack_trace() # Breakpoint for ipdb (needs to be installed), console-based
 
-    post = Post(path + POSTS_FILE_EXTENSION, root_dir='posts')
+    post = blog.get_post_or_404(path)
     return render_template('post.html', post=post)
     # Inject the function straight into the template, use as format_date(value)
     # return render_template('post.html', post=post, format_date=format_date)

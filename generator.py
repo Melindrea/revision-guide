c819fc80b3411bec8a6e9023f83f0790bd409292
Marie Hogebrandt <marie.hogebrandt@pineberry.com>
2015-07-12 14:17:57 +0200

    Lesson 2.2

--- generator.py
+++ generator.py
@@ -1,14 +1,32 @@
 from flask import Flask, render_template
+from werkzeug import cached_property
+import markdown
+
+POSTS_FILE_EXTENSION = '.md'
 
 app = Flask(__name__)
+
+class Post(object):
+    def __init__(self, path):
+        self.path = path
+
+    @cached_property
+    def html(self):
+        with open(self.path, 'r') as fin:
+            content = fin.read().strip()
+
+        return markdown.markdown(content)
 
 @app.route('/')
 def index():
     return 'Hello World'
 
-@app.route('/blog/post')
+@app.route('/blog/<path:path>')
 def post():
-    return render_template('post.html', post_content='Hello, World (from a template!)')
+    path = os.path.join('posts', path + POSTS_FILE_EXTENSION)
+
+    post = Post(path)
+    return render_template('post.html', post=post)
 
 if __name__ == '__main__':
     app.run(port=8000)

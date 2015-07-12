6e1a4abce0fb027ddcda9e6b5ac3f4d6af481ea3
Marie Hogebrandt <marie.hogebrandt@pineberry.com>
2015-07-12 15:54:51 +0200

    Lesson 2.4

--- generator.py
+++ generator.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from werkzeug import cached_property
-import markdown, os
+import markdown, os, yaml
 
 POSTS_FILE_EXTENSION = '.md'
 
@@ -9,11 +9,29 @@ app = Flask(__name__)
 class Post(object):
     def __init__(self, path):
         self.path = path
+        self._initialise_metadata()
+
+    def _initialise_metadata(self):
+        content = ''
+        first_separator = False
+
+        with open(self.path, 'r') as fin:
+            for line in fin:
+                if line.strip() == '---':
+                    if first_separator:
+                        break
+                    else:
+                        first_separator = True
+                        continue
+
+                content += line
+
+        self.__dict__.update(yaml.load(content))
 
     @cached_property
     def html(self):
         with open(self.path, 'r') as fin:
-            content = fin.read().strip()
+            content = fin.read().split('---', 2)[2].strip()
 
         return markdown.markdown(content)
 
--- posts/hello.md
+++ posts/hello.md
@@ -1,3 +1,8 @@
+---
+title: Hello World
+date: 2015-07-12
+---
+
 # Hello World
 
 This is a new thingy

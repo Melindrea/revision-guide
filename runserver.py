3a386c64818cb421d0b1ee03c3f064c8e418816f
Marie Hogebrandt <marie@pineberry.com>
2015-08-10 16:05:22 +0200

    Fixing freezer

--- runserver.py
+++ runserver.py
@@ -6,6 +6,11 @@ from blog import app
 
 freezer = Freezer(app)
 
+@freezer.register_generator
+def category_index():
+    for category in ['writing', 'language', 'narrative']:
+        yield {'category': category}
+
 if len(sys.argv) > 1 and sys.argv[1] == 'build':
     freezer.freeze()
 else:

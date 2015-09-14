43b224a128a3c9767c2bcb96b629e4d4ad6fb30b
Marie Hogebrandt <marie@pineberry.com>
2015-09-14 10:36:46 +0200

    New build

--- settings.py
+++
@@ -1,8 +0,0 @@
-FREEZER_BASE_URL = 'https://melindrea.github.io/revision-guide'
-FREEZER_DESTINATION = '../../build'
-FREEZER_DESTINATION_IGNORE = ['.git*']
-
-POSTS_FILE_EXTENSION = '.md'
-PAGES_FILE_EXTENSION = '.md'
-POSTS_DIRECTORY = 'content/posts'
-PAGES_DIRECTORY = 'content/pages'
